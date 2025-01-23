9a100b0f5a46813b27f9cb3186d00b5151219132
vivekKEUS <vivek.upadhyay@keus.in>
2025-01-23 17:29:30 +0530

    Add facts and trigger, and event subscription for fan automation

---
+++ plugin-fan/actions/GetFanSpeed.ts
@@ -0,0 +1,33 @@
+import type { Context } from "moleculer";
+import type { IResponse } from "../../types"
+import { FACTS, MODES, POWER, SPEEDS } from "../constants";
+export class GetFanSpeed {
+    static async handler(ctx: Context): Promise<IResponse> {
+        try {
+            //get the deviceID from ctx.params
+            let data: Record<string, any> = {}
+            let i = Math.random()
+            let deviceSpeed = "null"
+            if (i <= 0.33){
+                deviceSpeed = SPEEDS.LOW
+            }else if(i <= 0.66){
+                deviceSpeed = SPEEDS.MEDIUM
+            }else{
+                deviceSpeed = SPEEDS.HIGH
+            }
+            data[FACTS.MODE] = deviceSpeed;
+
+            return {
+                success: true,
+                data: data
+            }
+        } catch (err) {
+            console.log(err);
+            return {
+                success: false,
+                error: err?.toString()
+            }
+        }
+    }
+}
+
